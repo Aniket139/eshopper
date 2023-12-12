292caa1433e3e3e4897a5612238b1cba418be707
Aniket Patel <aniketpatel139@gmail.com>
2023-12-12 15:48:25 +0530

    change all file stucture

--- app/assets/javascript/application.js
+++
@@ -1,8 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require js/jquery
-//= require js/bootstrap.min
-//= require js/jquery.scrollUp.min
-//= require js/jquery.prettyPhoto
-//= require js/main
-import "@hotwired/turbo-rails";
-import "controllers";
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//=require js/jquery
+//=require js/bootstrap.min
+//=require js/html5shiv
+//=require js/jquery.scrollUp.min
+//=require js/price-range
+//=require js/jquery.prettyPhoto
+//=require js/main
