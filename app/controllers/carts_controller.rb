8a9b2c617506c4d8e7c6395ab5866b55d3f107fe
Aniket139 <aniketpatel139@gamil.com>
2024-01-08 11:05:26 +0530

    cart page created

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,14 @@
 class CartsController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: %i[create update]
   before_action :authenticate_customer!
   before_action :set_cart, only: %i[update destroy]
 
   def index
     @carts = current_customer.carts
+    @total_cart_price = @carts.sum { |item| item.product.price * item.quantity.to_i }
+    @eco_tax = @total_cart_price * 2 / 100
+    @shipping_cost = @total_cart_price > 1000 ? "free" : 50
+    @total_price = @total_cart_price + @eco_tax + @shipping_cost.to_i
   end
   
   def create
@@ -12,7 +16,11 @@ class CartsController < ApplicationController
     if @cart.save
       flash[:success] = "Product added to cart successfully!"
     else
-      flash[:alert] = "Product not add to cart"
+      if current_customer.carts.exists?(product_id: params[:product_id])
+        flash[:alert] = "Product already exists in the cart."
+      else
+        flash[:alert] = "Product not added to cart."
+      end
     end
     respond_to do |format|
       format.js { render inline: "window.location.reload();" }
@@ -20,10 +28,12 @@ class CartsController < ApplicationController
   end
 
   def update
-    if @cart.update(quantity: @cart.quantity.to_i + 1)
-      flash[:success] = "update"
-    else
-      flash[:alert] = "not update"
+    if params[:type] == "increase"
+      @cart.update(quantity: @cart.quantity.to_i + 1)
+      flash[:success] = "Quantity updated successfully!"
+    elsif params[:type] == "decrease" && @cart.quantity > 1
+      @cart.update(quantity: @cart.quantity.to_i - 1)
+      flash[:success] = "Quantity updated successfully!"
     end
     respond_to do |format|
       format.js { render inline: "window.location.reload();" }
