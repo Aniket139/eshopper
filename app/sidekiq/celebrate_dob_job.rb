59a6b27faa11c817e707cf5cae28b66b0d217adc
Aniket139 <aniketpatel139@gamil.com>
2024-02-08 18:40:19 +0530

    Revert "created dob in activejob"
    This reverts commit 97f6d714aebcdf232584959d11eee85749d7e2b3.

--- app/sidekiq/celebrate_dob_job.rb
+++ app/sidekiq/celebrate_dob_job.rb
@@ -5,9 +5,9 @@ class CelebrateDobJob
   include Sidekiq::Job
   queue_as :default
 
-  def perform
+  def perform(_args)
     @customers = Customer.all
-    @celebrates = @customers.select { |c| c.dob&.month == Date.today.month && c.dob&.day == Date.today.day }
+    @celebrates = @customers.select { |c| c.dob.month == Date.today.month && c.dob.day == Date.today.day }
     return unless @celebrates.any?
 
     @celebrates.each do |customer|
